bbc11abdd140709a19973b4c473355493b333fdd
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-06-25 11:57:22 +0600

    new commit

--- js/banking.js
+++ js/banking.js
@@ -1,10 +1,18 @@
+// Get deposit and withdraw input using function
+function getInputValue(inputid) {
+    const newInputAmount = document.getElementById(inputid);
+    const newInputText = newInputAmount.value;
+    const newInputValue = parseFloat(newInputText);
+
+    // Clear new input value 
+    newInputAmount.value = '';
+    return newInputValue;
+}
+
 // Get Event Handler on Deposit Button 
 document.getElementById('deposit-button').addEventListener('click', function () {
 
-    // Get deposit input value 
-    const newDepositInput = document.getElementById('input-deposit');
-    const newDepositInputText = newDepositInput.value;
-    const newDepositValue = parseFloat(newDepositInputText);
+    const newDepositValue = getInputValue('input-deposit')
 
     // Get Current Deposit Value 
     const currentDepostAmount = document.getElementById('deposit-total');
@@ -16,5 +24,40 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     currentDepostAmount.innerText = totalDepositAmount;
 
     // Get Balance Value 
-    const currentBalance = document.getElementById('balance-total')
+    const currentBalance = document.getElementById('balance-total');
+    const currentBalanceText = currentBalance.innerText;
+    const currentBalanceTotal = parseFloat(currentBalanceText);
+
+    // Adding current balance with new input deposit amount 
+    const totalBalance = currentBalanceTotal + newDepositValue;
+    currentBalance.innerText = totalBalance;
+})
+
+// Get withdraw event handler 
+document.getElementById('withdraw-button').addEventListener('click', function () {
+
+    // get withdraw input value 
+    // const newWithdrawInput = document.getElementById('input-withdraw');
+    // const newWithdrawInputText = newWithdrawInput.value;
+    // const newWithdrawTotal = parseFloat(newWithdrawInputText);
+
+    const newWithdrawTotal = getInputValue('input-withdraw')
+
+    // get current withdraw amount 
+    const currentWithdrawTotal = document.getElementById('withdraw-total');
+    const currentWithdrawText = currentWithdrawTotal.innerText;
+    const currentWithdrawTotalAmount = parseFloat(currentWithdrawText);
+
+    // Adding current withdraw value with new withdraw input value 
+    const newWithdrawTotalAmount = currentWithdrawTotalAmount + newWithdrawTotal;
+    currentWithdrawTotal.innerText = newWithdrawTotalAmount;
+
+    // Get Balance Total Amount 
+    const currentBalance = document.getElementById('balance-total');
+    const currentBalanceText = currentBalance.innerText;
+    const currentBalanceTotal = parseFloat(currentBalanceText);
+
+    // Subtraction from current Balance
+    const totalBalance = currentBalanceTotal - newWithdrawTotal
+    currentBalance.innerText = totalBalance;
 })
